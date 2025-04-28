d9e8bc8acfdb5c006b75f0d7e01cf57eb384d1dd
LoreviQ <lorevi@protonmail.com>
2025-04-28 21:35:23 +0100

    made image classifier class based

--- src/image/image_classification.py
+++ src/image/image_classification.py
@@ -2,12 +2,29 @@ import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
 import pickle
 
-def save_class_mapping(dataset_path, pickle_path):
-    """
-    Save the class mapping from the dataset to a pickle file.
-    """
-    initial_transform = transforms.Compose([])
-    full_dataset = ImageFolder(root=dataset_path, transform=initial_transform)
-    class_to_idx = full_dataset.class_to_idx
-    with open(pickle_path, 'wb') as f:
-        pickle.dump(class_to_idx, f)
+class ImageClassifier:
+    def __init__(self, dataset_path, pickle_path):
+        """
+        Initialize the ImageClassifier with the dataset path and pickle path.
+        """
+        self.dataset_path = dataset_path
+        self.pickle_path = pickle_path
+        
+
+    def save_class_mapping(self):
+        """
+        Save the class mapping from the dataset to a pickle file.
+        """
+        initial_transform = transforms.Compose([])
+        full_dataset = ImageFolder(root=self.dataset_path, transform=initial_transform)
+        class_to_idx = full_dataset.class_to_idx
+        with open(self.pickle_path, 'wb') as f:
+            pickle.dump(class_to_idx, f)
+
+    def load_class_mapping(self):
+        """
+        Load the class mapping from a pickle file.
+        """
+        with open(self.pickle_path, 'rb') as f:
+            class_to_idx = pickle.load(f)
+        return class_to_idx
--- src/train.py
+++ src/train.py
@@ -3,10 +3,10 @@ Script to train the image classification model using PyTorch.
 """
 
 import os
-from image.image_classification import save_class_mapping
+from image.image_classification import ImageClassifier
 
 dataset_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'training_images')
 pickle_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'class_to_idx.pkl')
 
 if __name__ == "__main__":
-    save_class_mapping(dataset_path, pickle_path)
+    image_classifier = ImageClassifier(dataset_path, pickle_path)
